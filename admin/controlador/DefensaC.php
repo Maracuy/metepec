b180a1718c05074e5d08353543c710641cfac0e7
Goderlive <goder@live.com>
2020-12-30 14:29:32 -0600

    Last Update antes de cambiar de rama

--- admin/controlador/DefensaC.php
+++ admin/controlador/DefensaC.php
@@ -9,14 +9,16 @@ class Defensa{
 
     function linkBorrar($id, $posicion){
 
-		return $link_delete = '<a href="deletedefensasql.php?cargo=' . $posicion . '&id=' . $id . '">' . '<i class="fas fa-trash blackiconcolor"></i> </a>';
+        return $link_delete = '<a href="deletedefensasql.php?cargo=' . $posicion . '&id=' . $id . '">' . '<i class="fas fa-trash blackiconcolor"></i> </a>';
 
     }
 
 
     function linkAgregar($id, $casilla){
-      	return $link_Add = '' . $id . '';
+        return $link_Add = '' . $id . '';
     }
+
+
 }
 
 ?>
--- admin/controlador/defensa.php
+++ admin/controlador/defensa.php
@@ -9,6 +9,8 @@ if (!$zonas) {
 	die();
 }
 include 'DefensaC.php';
+$ciudadano = New Defensa;
+
 ?>
 
 <h4>Área de administración de la Defensa del Voto</h4>
@@ -26,10 +28,20 @@ include 'DefensaC.php';
 	}else{
 		$nombre_rz = '';
 	}
+	$modal = '<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" onclick="numero(' . $zona['zona'] . ')" data-target="#exampleModal"> <i class="fas fa-user-plus"></i> </button>';
+
 	?>
 		
 	<div class="container-fluid bg-info bg-gradient text-light">
-		<h3>Zona: <?php echo $zona['zona']?></h3> <h6>RZ PRINCIPAL: <?php echo $nombre_rz ?> </h6>
+		<h3>Zona: <?php echo $zona['zona']?></h3> 
+		<h6>RZ PRINCIPAL: 
+			<?php 
+			if($zona['id_cordinador_zona_defenza'] != ''){
+				echo $nombre_rz; 
+			}else{
+				echo $modal;
+			}
+			?> </h6>
 
 			<?php
 				$stm = $con->query("SELECT * FROM representantes_generales WHERE id_zona = $id_zona");
@@ -63,9 +75,19 @@ include 'DefensaC.php';
 											<thead>
 												<tr>
 												<th scope="col">Puesto</th>
-												<th scope="col">Nombres</th>
-												<th scope="col">Apellidos</th>
-												<th scope="col">otros datos...</th>
+												<th scope="col">Col</th>
+												<th scope="col">Local</th>
+												<th scope="col">Prev</th>
+												<th scope="col">Comp</th>
+												<th scope="col">Nombre</th>
+												<th scope="col">UP</th>
+												<th scope="col">Origen</th>
+												<th scope="col">Afiliación</th>
+												<th scope="col">Beneficios</th>
+												<th scope="col">Colaboración</th>
+												<th scope="col">Capacitaciones</th>
+												<th scope="col">Cubre</th>
+												<th scope="col">Promo</th>
 												</tr>
 											</thead>
 											<tbody>
@@ -73,7 +95,6 @@ include 'DefensaC.php';
 													$stm = $con->query("SELECT * FROM puestos_defensa WHERE id_casilla = $id_casilla");
 													$puestos = $stm->fetchAll(PDO::FETCH_ASSOC);
 													foreach($puestos as $puesto):
-														$ciudadano = New Defensa;
 														$linkBorrar = $ciudadano->linkBorrar($puesto['id_ciudadano'], $puesto['tipo_puesto']);
 														$linkAgregar = $ciudadano;
 														$modal = '<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" onclick="numero(' . $puesto['id_puesto'] . ')" data-target="#exampleModal"> <i class="fas fa-user-plus"></i> </button>';
@@ -81,10 +102,13 @@ include 'DefensaC.php';
 												
 												<tr>
 												<td><?php echo $puesto['nombre_puesto'] ?></td>
-												<td><?php echo $name = ($puesto['id_ciudadano'] != '') ? $ciudadanos[$puesto['id_ciudadano']-1]['nombres'] : "" ?></td>
-												<td><?php echo $name = ($puesto['id_ciudadano'] != '') ? $ciudadanos[$puesto['id_ciudadano']-1]['apellido_p'] : "" ?></td>
-				
-												<td><?php echo $name = ($puesto['id_ciudadano'] != '') ? $ciudadanos[$puesto['id_ciudadano']-1]['apellido_m'] : "" ?></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td><?php echo $name = ($puesto['id_ciudadano'] != '') ? $ciudadanos[$puesto['id_ciudadano']-1]['nombres'] . " " . $ciudadanos[$puesto['id_ciudadano']-1]['apellido_p'] . " " .$ciudadanos[$puesto['id_ciudadano']-1]['apellido_m'] : "" ?></td>
+												<td></td>
+												<td></td>
 												<td><?php echo $link = ($puesto['id_ciudadano'] == '' ) ? $modal : $linkBorrar ?></td>
 
 												</tr>
@@ -106,18 +130,23 @@ include 'DefensaC.php';
 
 <script>
 var casilla;
+var rz;
 
 function borrarCiudadano(id) {
     if(confirm("my text here")) document.location = 'http://stackoverflow.com?id=' + id;
 }
 function numero(dato){
 	casilla = dato;
-	console.log(casilla);
+}
+function zona(dato){
+	rz = dato;
 }
 
-
+function AgregarRZ(id){
+    if(confirm("Seguro que desea agregarlo como RZ?")) document.location = 'controlador/adddefensasql.php?id=' + id +'&casilla=' + rz;
+}
 function AgregarCiudadano(id) {
-    if(confirm("Seguro que desea agregarlo?")) document.location = 'controlador/adddefensasql.php?id=' + id +'&casilla=' + casilla;
+    if(confirm("Seguro que desea agregarlo a la casilla")) document.location = 'controlador/adddefensasql.php?id=' + id +'&casilla=' + casilla;
 }
 </script>
 
